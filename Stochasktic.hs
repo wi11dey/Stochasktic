e751a3712d2d8b1278b1ee13ffdf2fc754ddafa2
Will Dey <wdey@palantir.com>
2024-04-15 00:26:19 -0400

    integration

--- Stochasktic.hs
+++ Stochasktic.hs
@@ -3,9 +3,32 @@
 
 import Control.Arrow
 
+data DifferentialEquation exp var
+
+instance ArrowLoop DifferentialEquation where
+  loop f = 
+
 data Integrated
 
-(∫) :: ArrowLoop a ⇒ a Expression Integrated
+data Time
+t = Time
+
+data Variable =
+  Time |
+  Wiener
+
+data Expression =
+  D Variable |
+  Integrated Expression |
+  Add Expression Expression |
+  Multiply Expression Expression |
+  Negate Expression |
+  Reciprocal Expression
+
+d = D
+(∫) :: DifferentialEquation Expression Expression
+(∫) = arr . Integrated
+at :: Floating f ⇒ Integrated → f → f
 
 blackScholes = proc w → do
   rec
