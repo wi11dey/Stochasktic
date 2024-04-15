81a7b3fc416b94cf8066d9be358741b787578fb1
Will Dey <wdey@palantir.com>
2024-04-15 00:59:36 -0400

    example

--- Stochasktic.hs
+++ Stochasktic.hs
@@ -15,7 +15,18 @@ t = Time
 
 data Variable =
   Time |
-  Wiener
+  WienerProcess Int
+
+class Inputs a where
+  inputs :: a
+
+instance Inputs (Variable) where
+  inputs = (WienerProcess 0)
+
+instance Inputs (Variable, Variable) where
+  inputs = (WienerProcess 0, WienerProcess 1)
+
+-- TODO tagless final arrow?
 
 data Expression =
   D Variable |
@@ -34,3 +45,10 @@ blackScholes = proc w → do
   rec
     price ←(∫)⤙ price * (μ dataset * d t + σ dataset * d w)
   process ⤙ price
+
+-- https://math.stackexchange.com/questions/1050770/how-to-solve-system-of-stochastic-differential-equations
+example = proc (w₁, w₂) → do
+  rec
+    n₁ ←(∫)⤙ (2*a - 1)*p*n₁*d t + α*n₁*d w₁
+    n₂ ←(∫)⤙ (2*p*n₁ - μ*n₂)*d t + α₂*n₂*d w₂
+  processes ⤙ [n₁, n₂]
